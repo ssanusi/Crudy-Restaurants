ffbca5bdc7d3bf61958f0ac2ae764b6b5a6f4965
sulaimansanusi <sulaiman.sanusi@icloud.com>
2020-03-05 11:01:26 +0100

    refactored seed data

--- src/main/java/com/ssanusi/crudyrestaurants/SeedData.java
+++ src/main/java/com/ssanusi/crudyrestaurants/SeedData.java
@@ -25,16 +25,16 @@ public class SeedData implements CommandLineRunner {
         r1.getMenus().add(new Menu("Mac and Cheese", 5.95, r1));
         restaurantService.save(r1);
 
-//        String rest2Name = "Chicken Republic";
-//        Restaurant r2 = new Restaurant(rest2Name, "456 Least Street", "Kaduna", "KD", "555-555-1234");
-//        r2.getMenus().add(new Menu("Mac and Cheese", 8.95, r2));
-//        restaurantService.save(r2);
-//
-//
-//        String rest3Name = "Apple Cafe";
-//        Restaurant r3 = new Restaurant(rest3Name, "678 Least Street", "Kano", "KN", "555-555-1234");
-//        r3.getMenus().add(new Menu("Mac and Cheese", 10.95, r3));
-//        restaurantService.save(r3);
+        String rest2Name = "Chicken Republic";
+        Restaurant r2 = new Restaurant(rest2Name, "456 Least Street", "Kaduna", "KD", "555-555-1234");
+        r2.getMenus().add(new Menu("Mac and Cheese", 8.95, r2));
+        restaurantService.save(r2);
+
+
+        String rest3Name = "Jade Palace Chinese Cuisine";
+        Restaurant r3 = new Restaurant(rest3Name, "678 Least Street", "Kano", "KN", "555-555-1234");
+        r3.getMenus().add(new Menu("Mac and Cheese", 10.95, r3));
+        restaurantService.save(r3);
 
 
         List<Restaurant> printList = restaurantService.findall();
